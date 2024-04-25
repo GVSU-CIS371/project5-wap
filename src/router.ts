bf1a47273ff17a8caf6dc1d82e5d9a39a613077c
Andrew Arent <90004565+AndrewACodes@users.noreply.github.com>
2024-04-24 22:15:15 -0400

    commit

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "./components/Home.vue";
+import Home from "./components/Home.vue";
+import Clothing from "./components/Clothing.vue";
+import Electronics from "./components/Electronics.vue";
+import Groceries from "./components/Groceries.vue";
+import BestSeller from "./components/BestSeller.vue";
+
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    props: true,
-    component: HomeView,
-  },
-  // you need to add more routes here
+  { path: "/", name: "home", component: Home },
+  { path: "/clothing", name: "clothing", component: Clothing },
+  { path: "/electronics", name: "electronics", component: Electronics },
+  { path: "/groceries", name: "groceries", component: Groceries },
+  { path: "/bestseller", name: "bestseller", component: BestSeller },
 ];
 
 const router = createRouter({
